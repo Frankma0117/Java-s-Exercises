3bc228b06ff20ca373843af86492b759993c0392
Frankma17 <frankin.canon@uptc.edu.co>
2023-01-01 17:54:33 -0500

    Correción rutas

---
+++ Basic_Level/src/Exercise_1.java
@@ -0,0 +1,17 @@
+public class Exercise_1 {
+
+    /**Declare two numeric variables (with the value you want). 
+     * Display by console the addtion , subtraction ,
+     * multiplication , division and module (remainder of the division).
+    **/
+    public static void main(String[] args) throws Exception {
+        int number1 = 3;
+        int number2 = 4;
+        System.out.println("Ejercicio 1 - Operaciones básicas");
+        System.out.println("El resultado de la suma de los dos numeros es : " + (number1+number2));
+        System.out.println("El resultado de la resta de los dos numeros es : " + (number1-number2));
+        System.out.println("El resultado de la multiplicación de los dos numeros es : " + number1*number2);
+        System.out.println("El resultado de la división de los dos numeros es : " + number1/number2);
+        System.out.println("El resultado del módulo de los dos numeros es : " + number1%number2);
+    }
+}
